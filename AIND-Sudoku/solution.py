8809be03b2b1e3c0488b3ffde055a59933b588d7
Alexandru Korotcov <akorotcov@yahoo.com>
2017-02-21 22:28:06 -0500

    Project updated
    AIND-Sudoku, Ran 3 tests, OK

--- AIND-Sudoku/solution.py
+++ AIND-Sudoku/solution.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[2]:
+
 assignments = []
 
 def assign_value(values, box, value):
@@ -131,6 +136,8 @@ def reduce_puzzle(values):
         values = eliminate(values)
         # Use the Only Choice Strategy
         values = only_choice(values)
+        # Use the Naked Twins Strategy
+        values = naked_twins(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
